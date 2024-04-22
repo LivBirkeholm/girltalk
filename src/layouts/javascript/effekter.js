4fe3c8bb08a3201bac500da71f77ee6c88f8cf98
sofiadalsgaard <soda0001@stud.kea.dk>
2024-04-22 15:11:46 +0200

    javascript

---
+++ src/layouts/javascript/effekter.js
@@ -0,0 +1,16 @@
+// Lyt efter scrollbegivenheder
+window.addEventListener("scroll", function () {
+  // Find højden på vinduet og dokumentet
+  var windowHeight = window.innerHeight;
+  var docHeight = document.body.clientHeight;
+  // Find scrollpositionen
+  var scrollPosition = window.scrollY || window.pageYOffset || document.documentElement.scrollTop;
+
+  // Hvis brugeren har scrollet til sektionen
+  if (scrollPosition > (docHeight - windowHeight) / 2) {
+    // Tilføj klassen til tekstelementet for at udløse animationen
+    document.getElementById("tekstElement").classList.add("animateElement");
+    // Tilføj klassen til billedet for at udløse animationen
+    document.getElementById("billedeElement").classList.add("animateElement");
+  }
+});
